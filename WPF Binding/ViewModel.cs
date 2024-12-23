1c5c75247d13ff9533d72b46c0ff7d50843dec31
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-23 18:37:48 +0000

    WPF simple 2 way binding
    Note we need the UpdateSourceTrigger for the update to happen on each keystroke

---
+++ WPF Binding/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Binding
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        string _name = "";
+        public string Name { 
+            get => _name;
+            set 
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
